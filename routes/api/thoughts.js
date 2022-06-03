c24a5590d29db6f79a30e949d0c704165fd0d69c
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-06-02 23:31:16 -0400

    inital file structure

---
+++ routes/api/thoughts.js
@@ -0,0 +1,17 @@
+
+// get to get all thoughts
+
+//  get to get a single thought by its _id
+
+// post to create a new thought (dont forget to push the created thoughts _id to the associated users thoughts array field) 
+
+// put to update a though by its _id
+
+// delete to remove a though by its _id
+
+
+// /api/thoughts/:thoughtId/reactions
+
+// POST to create a reaction stored in a single thought's reactions array field
+
+// DELETE to pull and remove a reaction by the reaction's reactionId value
---
+++ routes/api/users.js
@@ -0,0 +1,20 @@
+
+// get all users
+
+// get a single user by _id and populated thorough and friend data 
+
+// post a new user
+
+// put to update a user by its _id
+
+// delete to remove user but its _id
+
+
+// BONUS remote a user's associated thoughts when deleted 
+
+
+// /api/users/:userId/friends/:friendId
+
+// POST to add a new friend to a user's friend list
+
+// DELETE to remove a friend from a user's friend list
