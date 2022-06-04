84b57d48250929d28aeb85db999df2cfd46ab6a7
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-06-04 19:15:59 -0400

    cleanup though model code

--- models/Thought.js
+++ models/Thought.js
@@ -60,10 +60,6 @@ const ThoughtSchema = new Schema(
     }
 );
 
-// ReactionSchema.virtual('reactionCount').get(function() {
-//     return this.reactions.reduce((total, reaction) => total + reaction.length +1, 0);
-// });
-
 ThoughtSchema.virtual("reactionCount").get(function () {
     return this.reactions.length;
 });
@@ -71,45 +67,3 @@ ThoughtSchema.virtual("reactionCount").get(function () {
 const Thought = model('Thoughts', ThoughtSchema);
 
 module.exports = Thought;
-
-// thoughtText
-// String
-// Required
-// Must be between 1 and 280 characters
-
-// createdAt
-// Date
-// Set default value to the current timestamp
-// Use a getter method to format the timestamp on query
-
-// username (The user that created this thought)
-// String
-// Required
-
-// reactions (These are like replies)
-// Array of nested documents created with the reactionSchema
-
-// Schema Settings
-// Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
-
-
-// reactionId
-// Use Mongoose's ObjectId data type
-// Default value is set to a new ObjectId
-
-// reactionBody
-// String
-// Required
-// 280 character maximum
-
-// username
-// String
-// Required
-
-// createdAt
-// Date
-// Set default value to the current timestamp
-// Use a getter method to format the timestamp on query
-
-// Schema Settings
-// This will not be a model, but rather will be used as the reaction field's subdocument schema in the Thought model.
