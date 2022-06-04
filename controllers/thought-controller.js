99292e986323122317cabecaedf6b0206dd66e83
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-06-04 10:48:32 -0400

    start thought-controller

---
+++ controllers/thought-controller.js
@@ -0,0 +1,16 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+    // add new thought 
+
+
+    // add new reaction
+
+
+    // remove thought 
+
+
+    // remove reaction 
+}
+
+module.exports = thoughtController; 
