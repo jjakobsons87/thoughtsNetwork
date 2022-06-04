95dc004a58ed387816d44e472fadceaa31dd9bc6
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-06-04 15:21:47 -0400

    update thought working

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -48,6 +48,19 @@ const thoughtController = {
         .catch(err => res.json(err));
     },
 
+    // update thought
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate({ _id: params.thoughtId }, body, { new: true, runValidators: true })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No Thought found with this id!' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
     // add new reaction
     addReaction({ params, body }, res) {
         Thought.findOneAndUpdate(
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { getAllThoughts, getThoughtById, createThought, removeThought, addReaction, removeReaction } = require('../../controllers/thought-controller.js');
+const { getAllThoughts, getThoughtById, createThought, removeThought, addReaction, removeReaction, updateThought } = require('../../controllers/thought-controller.js');
 // need NEWREACTION and REMOVEREACTION 
 
 // get all thoughts
@@ -11,18 +11,14 @@ router.route('/:thoughtId').get(getThoughtById)
 // create thought 
 router.route('/:userId').post(createThought);
 
+// update thought 
+router.route('/:userId/:thoughtId').put(updateThought);
+
 // remove thought
 router.route('/:userId/:thoughtId').delete(removeThought);
 
 // add reaction 
 router.route('/:userId/:thoughtId').put(addReaction).delete(removeReaction);
-
-// update thought 
-// router.route('/:userId/:thoughtId').put(updateThought);
-
-
-// post to create a new thought (dont forget to push the created thoughts _id to the associated users thoughts array field) 
-
 
 // /api/thoughts/:thoughtId/reactions - post new and delete one 
 router.route('/:thoughtId/reactions').post(addReaction).delete(removeReaction);
