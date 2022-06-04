2622dd45d8580d5486a209ec5858dcc71b3f2625
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-06-04 10:37:27 -0400

    add find user by id and create user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -19,10 +19,32 @@ const userController = {
     },
 
     // get user by id 
-
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .populate({
+            path: 'comments',
+            select: '-__v'
+        })
+        .select('-__v')
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No User found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
 
     // create a user 
-
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));
+    },
 
     // update user by id 
 
